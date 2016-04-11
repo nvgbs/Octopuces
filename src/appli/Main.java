898a13322fdd75b20579fcef24defa6801541a24
MisterNono <noelmaurice4@gmail.com>
2016-04-11 17:16:35 +0200

    Sauvegarde

---
+++ src/appli/Main.java
@@ -0,0 +1,39 @@
+package appli;
+
+
+
+import org.hibernate.cfg.Configuration;
+
+import base.BaseInit;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Boolean result = null;
+
+		
+		try
+		{
+			// configure hibernate
+			new Configuration().configure().buildSessionFactory();
+			
+			BaseInit.initData();
+			
+			result = true;
+		}
+		catch (Exception e)
+		{
+			result = false;
+
+			e.printStackTrace();
+		}
+		finally
+		{
+			System.out.println(String.valueOf(result));
+		}
+
+	}
+
+}
